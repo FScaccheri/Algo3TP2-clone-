b824de5b065dd6fc8c98aaabc80f678529684682
seblaz <seby_1996@hotmail.com>
2017-11-09 14:33:32 -0300

    Refactoring y se agregan tests. Trabajo de todo el equipo.

--- test/modeloTest/JugadorTest.java
+++ test/modeloTest/JugadorTest.java
@@ -9,7 +9,7 @@ import modelo.Jugador;
 public class JugadorTest {
 
 	@Test
-	public void AlAumentarElCapitalEn50000AumentaCorrectamente() {
+	public void AlAumentarElCapitalUnaVezAumentaCorrectamente() {
 		Jugador jugador = new Jugador();
 		double capitalInicial = jugador.getCapital();
 		
@@ -19,14 +19,34 @@ public class JugadorTest {
 	}
 
 	@Test
-	public void AlAumentarElCapitalEn10000AumentaCorrectamente() {
+	public void AlAumentarElCapitalDosVecesConValoresDistintosAumentaCorrectamente() {
 		Jugador jugador = new Jugador();
 		double capitalInicial = jugador.getCapital();
 		
+		jugador.aumentarCapital(50000);
 		jugador.aumentarCapital(10000);
 		
-		assertEquals(capitalInicial + 10000, jugador.getCapital(), 0);
+		assertEquals(capitalInicial + 60000, jugador.getCapital(), 0);
 	}
 
-	// Definir que ocurre si se aumenta el capital en un monto negativo o cero. Lanzamos excepcion o no es un escenario a considerar en nuestro modelo?
+	@Test
+	public void AlDisminuirElCapitalUnaVezAumentaCorrectamente() {
+		Jugador jugador = new Jugador();
+		double capitalInicial = jugador.getCapital();
+		
+		jugador.disminuirCapital(50000);
+		
+		assertEquals(capitalInicial - 50000, jugador.getCapital(), 0);
+	}
+
+	@Test
+	public void AlDisminuirElCapitalDosVecesConValoresDistintosAumentaCorrectamente() {
+		Jugador jugador = new Jugador();
+		double capitalInicial = jugador.getCapital();
+		
+		jugador.disminuirCapital(50000);
+		jugador.disminuirCapital(10000);
+		
+		assertEquals(capitalInicial - 60000, jugador.getCapital(), 0);
+	}
 }
--- test/modeloTest/Quini6Test.java
+++ test/modeloTest/Quini6Test.java
@@ -15,19 +15,19 @@ public class Quini6Test {
 
 	@Test
 	public void CuandoUnJugadorCaeEnQuini6PorPrimeraVezSuCapitalAumentaEn50000() {
-		Efecto quini6 = new Quini6();
+		Quini6 quini6 = new Quini6();
 		Jugador jugador = new Jugador();
 		
 		double capitalInicial = jugador.getCapital();
-		jugador.setCapital(0);
+
 		quini6.activarEfecto(jugador);
-		
-		assertEquals( 50000 , jugador.getCapital(), 0);
+
+		assertEquals( capitalInicial + 50000 , jugador.getCapital(), 0);
 	}
 
 	@Test
 	public void CuandoUnJugadorCaeEnQuini6PorSegundaVezSuCapitalAumentaEn30000() {
-		Efecto quini6 = new Quini6();
+		Quini6 quini6 = new Quini6();
 		Jugador jugador = new Jugador();
 		
 		quini6.activarEfecto(jugador);
@@ -41,7 +41,7 @@ public class Quini6Test {
 
 	@Test
 	public void CuandoUnJugadorCaeEnQuini6PorTerceraVezSuCapitalNoAumenta() {
-		Efecto quini6 = new Quini6();
+		Quini6 quini6 = new Quini6();
 		Jugador jugador = new Jugador();
 		
 		quini6.activarEfecto(jugador);
@@ -54,4 +54,23 @@ public class Quini6Test {
 		assertEquals(capitalInicial, jugador.getCapital(), 0);
 	}
 	
+	@Test
+	public void CuandoTresJuagadoresCaeEnQuini6PorPrimeraVezSuCapitalAumentaCorrectamente() {
+		Quini6 quini6 = new Quini6();
+		Jugador jugador1 = new Jugador();
+		Jugador jugador2 = new Jugador();
+		Jugador jugador3 = new Jugador();
+		
+		double capitalInicial1 = jugador1.getCapital();
+		double capitalInicial2 = jugador2.getCapital();
+		double capitalInicial3 = jugador2.getCapital();
+		
+		quini6.activarEfecto(jugador1);
+		quini6.activarEfecto(jugador2);
+		quini6.activarEfecto(jugador3);
+
+		assertEquals( capitalInicial1 + 50000 , jugador1.getCapital(), 0);
+		assertEquals( capitalInicial2 + 50000 , jugador2.getCapital(), 0);
+		assertEquals( capitalInicial3 + 50000 , jugador3.getCapital(), 0);
+	}
 }
