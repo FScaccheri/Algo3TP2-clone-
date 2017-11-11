d92969942767ce1d8b5e930c7807402a7cb6940e
FrancoR <friborati@fi.uba.ar>
2017-11-11 17:09:42 -0300

    Agregada una primera funcionalidad a tablero

--- src/modelo/Tablero.java
+++ src/modelo/Tablero.java
@@ -1,5 +1,20 @@
 package modelo;
 
+import java.util.LinkedList;
 public class Tablero {
-
+	
+	private LinkedList<Casillero> casilleros;
+	
+	public Tablero() {
+		casilleros = new LinkedList<Casillero>();
+	};
+	
+	public void agregar(Casillero unCasillero) {
+		casilleros.add(unCasillero);
+	};
+	
+	//La posicion del primer casillero añadido, es 0, es decir, la del segundo es 1, y asi sucesivamente;
+	public int posicion(Casillero unCasillero) {
+		return casilleros.indexOf(unCasillero);
+	}
 }
