e16aea95c70181ccebf28ed2775540f850247aee
FrancoR <friborati@fi.uba.ar>
2017-11-11 17:09:18 -0300

    Agregada una primera funcionalidad para Tablero

---
+++ test/modeloTest/TableroTest.java
@@ -0,0 +1,31 @@
+package modeloTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import modelo.*;
+
+public class TableroTest {
+
+	@Test
+	public void testAgregadoUnCasilleroAlTableroPuedoConocerSuOrden() {
+		
+		Tablero unTablero = new Tablero();
+		Casillero casillero1 = new Casillero();
+		Casillero casillero2 = new Casillero();
+		Casillero casillero3 = new Casillero();
+		Casillero casillero4 = new Casillero();
+		
+		unTablero.agregar(casillero1);
+		unTablero.agregar(casillero2);
+		unTablero.agregar(casillero3);
+		unTablero.agregar(casillero4);
+		
+		assertTrue(unTablero.posicion(casillero1) == 0);
+		assertTrue(unTablero.posicion(casillero2) == 1);
+		assertTrue(unTablero.posicion(casillero3) == 2);
+		assertTrue(unTablero.posicion(casillero4) == 3);
+	}
+
+}
