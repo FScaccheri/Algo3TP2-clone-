9cc102ec5223bec2c7ef044c234b6e0a5f579233
FScaccheri <francocass2009@hotmail.com>
2017-11-11 23:35:37 -0300

    Implementacion de Policia y algunos metodos provisionales de Jugador

--- src/modelo/Jugador.java
+++ src/modelo/Jugador.java
@@ -3,6 +3,7 @@ package modelo;
 public class Jugador {
 	double capital = 0;
 	int ultimaTirada = 0;
+	private int posicion;
 	
 	public double getCapital() {
 		return capital;
@@ -25,6 +26,19 @@ public class Jugador {
 	//VER SI ESTE METODO TIENE SENTIDO LUEGO DE PROGRAMAR EL MOVIMIENTO(SE USA EN SERVICIO)
 	public int getUltimaTirada() {
 		return ultimaTirada;
+	}
+
+	public int getPosicion() {
+
+		return this.posicion ;
+	}
+
+	//Metodo provisional para "mover" al Jugador, hasta que se implemente correctamente
+	//el movimiento. @Franco S.
+	public void moverAPosicion(int nuevaPosicion) {
+		
+		this.posicion = nuevaPosicion;
+		
 	}
 
 }
---
+++ src/modelo/Policia.java
@@ -0,0 +1,19 @@
+package modelo;
+
+public class Policia implements Activable{
+
+
+	private static final int POSICIONCARCEL = 5;
+
+	public void activarEfecto(Jugador jugador) {
+
+		jugador.moverAPosicion(POSICIONCARCEL);
+		
+	}
+
+	public int getPosicionCarcel() {
+		
+		return POSICIONCARCEL;
+	}
+
+}
