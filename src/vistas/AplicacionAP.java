0377b2354bf31891826d4f72a8bf146640111391
FScaccheri <francocass2009@hotmail.com>
2017-11-19 23:06:50 -0300

    Creada una primera botonera (que no hace nada) para la pantalla de juego

--- src/vistas/AplicacionAP.java
+++ src/vistas/AplicacionAP.java
@@ -25,33 +25,13 @@ public class AplicacionAP extends Application{
 		
 		stage.setTitle("AlgoPoly - Grupo 3(?)");
 		
-		// Contenedor (BorderPane) de la pantalla de juego
-		BorderPane contenedorJuego = new BorderPane();
+	
+		ContenedorJuego contenedorJuego = new ContenedorJuego(stage);
 		Scene escenaPrincipal = new Scene(contenedorJuego , 1000, 700);
-		
 		
 		ContenedorBienvenida contenedorBienvenida = new ContenedorBienvenida(stage, escenaPrincipal);
 		Scene escenaBienvenida =  new Scene(contenedorBienvenida, 1000, 700);	
 		
-		
-		
-		
-		//Contenedor (VBox) de la pantalla de tablero
-		VBox contenedorTablero = new VBox();
-		// Seteo de imagen de fondo (tablero)
-		Image tablero = new Image("file:src/vistas/imagenes/monopoly board.jpg");
-		BackgroundImage imagenTablero = new BackgroundImage(tablero, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
-		// Seteo de imagenes de jugadores (sus piezas)
-		Image imagenPieza = new Image("file:src/vistas/imagenes/monopoly racecar.jpg");
-		ImageView vistaPieza = new ImageView();
-		vistaPieza.setImage(imagenPieza);
-		contenedorTablero.setBackground(new Background(imagenTablero));
-		contenedorTablero.getChildren().add(vistaPieza);
-		contenedorJuego.setCenter(contenedorTablero);
-		
-		
-		
-	
 		stage.setScene(escenaBienvenida);
 		
 		stage.show();
